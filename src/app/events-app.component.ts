dd629aecd2e385bfab3403f57655ff84cd6086aa
Ardouz11 <rachidardouz11@gmail.com>
2021-02-14 15:41:20 +0100

    first one

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+ // templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  template:`
+  <h2>Hello from Ardouz s first app</h2>
+  <img src="/assets/images/basic-shield.png">
+  `
+})
+export class EventsAppComponent {
+  title = 'Ardouz App';
+}
