0f9a9d8cc95e9890559ad6aed78a17d1f13e5ade
Ardouz11 <rachidardouz11@gmail.com>
2021-02-16 17:04:22 +0100

    module 4

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -3,18 +3,25 @@ import {Component,Input, Output,EventEmitter} from '@angular/core'
     selector:'event-thumbnail',
     template:`
     <div class="well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-    <div> Date:{{event.date}}</div>
-    <div> Time:{{event.time}}</div>
-    <div> Price:{{event.price}}\$</div>
-    <div>
-    <span>location :{{event.location.address}}</span>
-    <span class="pad-left">{{event.location.city}},{{event.location.country}}</span>
+    <h2>{{event?.name}}</h2>
+    <div> Date:{{event?.date}}</div>
+    <div [ngSwitch]="event?.time"> 
+    Time:{{event?.time}}
+    <span *ngSwitchCase="'8:00 am'">(Early Start) </span>
+    <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+    <span *ngSwitchDefault>(Normal Start)</span>
     </div>
-    <button class="btn btn-primary" (click)="handleClickMe()">Click Me </button>
-
+    <div> Price:{{event?.price}}\$</div>
+    <div *ngIf="event?.location">
+    <span>location :{{event?.location?.address}}</span>
+    <span class="pad-left">{{event?.location?.city}},{{event?.location?.country}}</span>
+    </div>
+    <div *ngIf="event?.onlineUrl">
+    Online URL:{{event?.onlineUrl}}
+    </div>
     </div>`,
     styles:[`
+    .thumbnail {min-height:210px}
     .pad-left{margin-left:10px;}
     .well div{color:#bbb;}
     `]
