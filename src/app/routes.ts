ef45fc97f269ed0b3255dd87125f692ec154afc8
Ardouz11 <rachidardouz11@gmail.com>
2021-02-18 16:29:40 +0100

    Commit Routing Part1

---
+++ src/app/routes.ts
@@ -0,0 +1,16 @@
+import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { EventsListComponent } from "./events/events-list-component";
+import {Routes} from '@angular/router'
+import { CreateEventComponent } from "./events/create-event.component";
+import { Error404Component } from "./errors/404.component";
+import { EventRouteActivator } from "./events/event-details/event-router-activator.service";
+import { EventListResolver } from "./events/shared/event-list-resolve.service";
+export const appRoutes:Routes=[
+    {path:'events/create', component:CreateEventComponent,canDeactivate:['canDeactivateCreateEvent']},
+    {path:'events',component:EventsListComponent,resolve:{events:EventListResolver}},
+    {path:'events/:id',component:EventDetailsComponent,canActivate:[EventRouteActivator]},
+    {path:'',redirectTo:'/events',pathMatch:'full'},
+    {path:'404', component:Error404Component}
+ 
+
+]
