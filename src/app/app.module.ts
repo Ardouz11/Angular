b6192c20f9461cf4fcc56e48564b3e778f32a8d8
Ardouz11 <rachidardouz11@gmail.com>
2021-02-19 17:40:50 +0100

    Commit Module 5 Part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{
+  NavBarComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  Error404Component,
+  EventRouteActivator,
+  EventListResolver,
+  EventsListComponent
+}from './events/index'
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list-component';
-import { EventThumbnailComponent } from './events/event-thumbnail-component';
-import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrModule } from 'ngx-toastr'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import {appRoutes} from './routes'
 import { RouterModule } from '@angular/router';
-import { CreateEventComponent } from './events/create-event.component';
-import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-router-activator.service';
-import { EventListResolver } from './events/shared/event-list-resolve.service';
 @NgModule({
   imports: [
     BrowserModule,
